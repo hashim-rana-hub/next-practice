b094d7437cf3e46f2cc0802608b61183e1b2c76c
Hashim Rana <ahashimrana@gmail.com>
2024-12-19 15:16:13 +0500

    styles the posts

--- src/app/posts/PostsList.jsx
+++ src/app/posts/PostsList.jsx
@@ -27,11 +27,14 @@ export default function PostsList({ initialData }) {
   }
 
   return (
-    <div>
+    <div className=" w-[80%] mx-auto">
       <h1>Posts</h1>
       <ul>
         {posts.map((post) => (
-          <li key={post.id} className="mb-4">
+          <li
+            key={post.id}
+            className="mb-4 border border-[#fff] px-2 py-1 rounded-lg text-[#000] bg-[#fff] bg-opacity-50"
+          >
             <h2 className="font-bold">{post.title}</h2>
             <p>{post.body}</p>
           </li>
